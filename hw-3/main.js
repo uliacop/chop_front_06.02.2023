763d5658a7a9e6264a2a8a19a4f728c6e76e238a
uliacop <uliacop@gmail.com>
2023-02-14 00:01:15 +0200

    deleted sign

--- hw-3/main.js
+++ hw-3/main.js
@@ -1,4 +1,4 @@
 'use strict'
 let firstNumber = +prompt ('Введіть перше число');
 let secondNumber = +prompt ('Введіть друге число');
-alert(`Користувач ввів ${firstNumber} і ${secondNumber}:\n${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber};\n${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber};\n${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber};\n${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber};`);
+alert(`Користувач ввів ${firstNumber} і ${secondNumber}:\n${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}\n${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}\n${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}\n${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
