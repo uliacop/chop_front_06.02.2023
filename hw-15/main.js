202c3d9997705bf134225789a780545ad063cde5
uliacop <uliacop@gmail.com>
2023-03-11 17:40:25 +0200

    modified hw-15

--- hw-15/main.js
+++ hw-15/main.js
@@ -18,15 +18,22 @@ const shoppingCart = {
            this.items.push(product);
       },
       deleteItem(productName) {
-          const { items} = this;
-          for (let i = 0; i < items.length; i += 1) {
-              const { name } = items [i];
-              if (productName === name) {
-                  console.log(i);
-                  items.splice(i, 1);
-              }
-          }       
-      },
+      const {items} = this;
+       for (let i = 0; i < items.length; i+= 1) {
+            const {name} = items[i];
+             if (productName.name === name) {
+                console.log('нашли такой продукт');
+                 if (items[i].count > 1) {
+                    items[i].count -= 1;
+                    console.log(i);
+                    return;
+                }
+                console.log(i);
+                items.splice(i, 1);
+                return;
+            }
+        }
+    },
       getTotalSum(){
         const { items } = this;
         let total = 0;
@@ -34,10 +41,9 @@ const shoppingCart = {
             total += price * count;
         }
           return total;
-
     },
     };
-  
+  shoppingCart.addItem({name: 'Mobile phone', count: 1, price: 400});  
   shoppingCart.addItem({name: 'Mobile phone', count: 1, price: 400});  
   shoppingCart.addItem({name: 'Electric kettle', count: 1, price: 300});  
   shoppingCart.addItem({name: 'Electric kettle', count: 1, price: 300});  
@@ -46,8 +52,12 @@ const shoppingCart = {
   shoppingCart.addItem({name: 'Dishwasher', count: 1, price: 200});  
   shoppingCart.addItem({name: 'Dishwasher', count: 1, price: 200});  
   console.log(shoppingCart.items);
-  shoppingCart.deleteItem('Mobile phone');
+  shoppingCart.deleteItem({name: 'Mobile phone', count: 1, price: 400});
+  shoppingCart.deleteItem({name: 'Dishwasher', count: 1, price: 200});
+  shoppingCart.deleteItem({name: 'Electric kettle', count: 1, price: 300});
   console.log(shoppingCart.items);
   console.log(shoppingCart.getTotalSum());
+
+
 
 
