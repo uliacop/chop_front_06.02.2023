4c87ceee1a0af2c69c39ac29294808c6c48588ec
uliacop <uliacop@gmail.com>
2023-04-18 15:10:01 +0300

    added hw-25

---
+++ hw-25/calculator.js
@@ -0,0 +1,14 @@
+let addNumber = (num1, num2) => {
+  return num1 + num2;
+};
+let substractNumber = (num1, num2) => {
+  return num1 - num2;
+};
+let multiplyNumber = (num1, num2) => {
+  return num1 * num2;
+};
+
+let divideNumber = (num1, num2) => {
+  return num1 / num2;
+};
+export { addNumber, substractNumber, multiplyNumber, divideNumber };
